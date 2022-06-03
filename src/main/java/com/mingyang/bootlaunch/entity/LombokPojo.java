a2e643575e702a95e71a1baf358b7522349578e5
Dotclv <yumingyang0212@163.com>
2022-06-03 17:30:39 +0800

    Lombok

---
+++ src/main/java/com/mingyang/bootlaunch/entity/LombokPojo.java
@@ -0,0 +1,16 @@
+package com.mingyang.bootlaunch.entity;
+
+import lombok.Data;
+
+/**
+ * @author: ymy
+ * @program: boot-lanuch
+ * @description: lomback 测试
+ * @date: 2022/6/3 17:28
+ * @version: 1.0
+ */
+@Data
+public class LombokPojo {
+    private String name;
+    private Integer age;
+}
