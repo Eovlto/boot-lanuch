8a23ddef93711fedb3ddc0d7c3f378842f03ca7e
Dotclv <yumingyang0212@163.com>
2022-06-04 17:13:30 +0800

    切面日志

--- src/main/java/com/mingyang/bootlaunch/controller/HelloController.java
+++ src/main/java/com/mingyang/bootlaunch/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.mingyang.bootlaunch.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date: 2022/6/3 17:04
  * @version: 1.0
  */
+@Api(tags = "测试接口")
 @RestController
 public class HelloController {
 
@@ -19,6 +22,7 @@ public class HelloController {
      * @return
      */
     @GetMapping("/hello")
+    @ApiOperation("hello")
     public String hello(String name) {
         return "hello haha :"+ name;
     }
