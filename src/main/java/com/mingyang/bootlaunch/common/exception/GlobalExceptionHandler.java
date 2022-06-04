1d7c882787e3fd0ff9a92da7be08428a160fdd0b
Dotclv <yumingyang0212@163.com>
2022-06-04 16:13:10 +0800

    统一日志

--- src/main/java/com/mingyang/bootlaunch/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mingyang/bootlaunch/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.mingyang.bootlaunch.common.exception;
 
 import com.mingyang.bootlaunch.common.result.Result;
 import com.mingyang.bootlaunch.common.result.ResultCode;
+import com.mingyang.bootlaunch.common.utils.ExceptionUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,13 +12,14 @@ import org.springframework.web.client.HttpClientErrorException;
 import static com.mingyang.bootlaunch.common.result.Result.failure;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     /**-------- 通用异常处理方法 --------**/
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public Result error(Exception e) {
-        e.printStackTrace();
+        log.error(ExceptionUtil.getMessage(e));
         return failure(ResultCode.INTERNAL_SERVER_ERROR);
     }
 
@@ -24,14 +27,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NullPointerException.class)
     @ResponseBody
     public Result error(NullPointerException e) {
-        e.printStackTrace();
+        log.error(ExceptionUtil.getMessage(e));
         return failure(ResultCode.PARAM_IS_EMPTY);
     }
 
     @ExceptionHandler(HttpClientErrorException.class)
     @ResponseBody
     public Result error(IndexOutOfBoundsException e) {
-        e.printStackTrace();
+        log.error(ExceptionUtil.getMessage(e));
         return failure(ResultCode.NOT_FOUND);
     }
 
@@ -39,7 +42,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BuzException.class)
     @ResponseBody
     public Result error(BuzException e) {
-        e.printStackTrace();
+        log.error(ExceptionUtil.getMessage(e));
         return failure(ResultCode.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 }
